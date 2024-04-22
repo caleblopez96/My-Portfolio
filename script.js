4b6a9e54ab25a9d4ba1117521af7a75ce979c30a
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-04-21 20:19:00 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.querySelector('button');
 const body = document.querySelector('body');
 const path = document.querySelector('path');
 
-button.addEventListener('click', (e) => {
+button.addEventListener('click', () => {
   body.classList.toggle('body-day');
   path.classList.toggle('svg-day');
 }); 
